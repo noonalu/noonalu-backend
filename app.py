0397d160f473978a7b8967055e6fadfe1cb7c744
Jeremy Rudman <jeremyrudman@gmail.com>
2022-11-19 23:00:58 -0500

    added some basic reading from a mongo

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+from os import environ 
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
+client = MongoClient(environ.get("MONGO_URI"))
+db = client.noonalu
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    calenders_collection = db['calender']
+    return list(calenders_collection.find({}))
+
 
 
 if __name__ == '__main__':
