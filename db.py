18822893e4c3dfd5039eb2308cee2bc79c5477e6
nxb9626 <nxb9626@rit.edu>
2022-12-24 23:06:22 -0500

    refactored database conneciton into its own class so that it's easier to maintain and close
    also added  a few extra comments

--- db.py
+++ db.py
@@ -5,7 +5,6 @@ from pymongo import MongoClient
 import logging as log
 import json
 from dotenv import load_dotenv
-import sys
 
 from pymongo.database import Database
 
@@ -26,62 +25,83 @@ def get_mock_data():
 
 
 ##################################################################
+class MongConn:
+    def __init__(self) -> None:
+        """Opens a connection to the database and"""
+        self.mongo_client = MongoClient(
+            environ.get("MONGO_URI"), int(environ.get("MONGO_PORT"))
+        )
 
+    def get_db(self) -> Database[Mapping[str, Any] | Any]:
+        """Returns noonalu database"""
+        noonalu_db = self.mongo_client.get_database("noonalu")
+        return noonalu_db
 
-def open_connection() -> Database[Mapping[str, Any] | Any]:
-    load_dotenv()
-    mongo_client = MongoClient(environ.get("MONGO_URI"), int(environ.get("MONGO_PORT")))
-    return mongo_client.get_database("noonalu")
+    def close(self) -> None:
+        """Closes database connection"""
+        self.mongo_client.close()
 
 
 def get_event_users(event_tag: str) -> dict:
     """return dict of users for event"""
-    return get_event_data(event_tag=event_tag)["users"]
+    return get_event(event_tag=event_tag)["users"]
 
 
 def get_event_availability(event_tag: str) -> dict:
     """returns calendar for event"""
-    return get_event_data(event_tag=event_tag)["availability"]
+    return get_event(event_tag=event_tag)["availability"]
 
 
-def _list_all_events():
-    conn = open_connection()
-    coll = conn.get_collection("events")
-    coll.create_index("tag")
-    return list(coll.find())
+def _read_all_events() -> list:
+    """Returns a list of all events"""
+    conn = MongConn()
+    noon_data = conn.get_db()
+    coll = noon_data.get_collection("events")
+    res = coll.find(sort=[("tag", 1)])
+    conn.close()
+    return list(res)
 
 
 def create_new_event():
-    conn = open_connection()
+    """Creates a new event, returns the tag for the event"""
+    conn = MongConn()
+    noon_data = conn.get_db()
     tag = utils.get_new_event_tag()
     event = {
         "tag": tag,
         "availability": {},
         "users": [],
     }
-    coll = conn.get_collection("events")
-    coll.insert_one(event)
+    event_collection = noon_data.get_collection("events")
+    event_collection.insert_one(event)
+    conn.close()
     log.info(event)
     return tag
 
 
-def get_event_data(event_tag):
-    conn = open_connection()
-    coll = conn.get_collection("events")
+def get_event(event_tag: str):
+    """returns dictionary containing event tied to the data"""
+    conn = MongConn()
+    noon_data = conn.get_db()
+
+    coll = noon_data.get_collection("events")
     res = coll.find_one({"tag": event_tag})
+
+    conn.close()
     return dict(res)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     log.basicConfig(level=log.DEBUG, stream=sys.stdout)
     tag = create_new_event()
-    assert get_event_data(event_tag=tag)["tag"] == tag
+    assert get_event(event_tag=tag)["tag"] == tag
     assert get_event_availability(tag) == {}
     assert get_event_users(tag) == []
 
     import pprint
 
-    pprint.pp(_list_all_events())
+    pprint.pp(_read_all_events())
     # client = MongoClient("localhost", 27017)
     # pprint.pp(get_event_data("qqeV6u8"))
 
